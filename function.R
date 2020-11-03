78110e3c9d67bec940ed7a29d43aa33d8b2a79e3
grashoff <grashoff@nd.edu>
2020-11-03 14:26:47 -0500

    added if, else expressions to check for columns not having 50 observations

--- function.R
+++ function.R
@@ -8,7 +8,15 @@ coofvar <- function (direc, column, forcecalc = FALSE)
   for (i in 1:numFiles)
   {
     tempcsv <- read.csv(file = fileNames[i], header = FALSE)
+    if (length(tempcsv[, column])<50 && forcecalc = FALSE)
+    {
+      return("One of the files has a column with less than the recommended 50 observations")
+    }
+    else
+    {
     covar = cv(tempcsv[,column])
     finalVec[i] <- covar
+    }
   }
+  return(finalVec)
 }
